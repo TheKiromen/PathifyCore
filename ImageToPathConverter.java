529efc3b2b2062702e3c23f697f493d5b90ad48f
TheKiromen <TheKiromen@gmail.com>
2021-05-17 00:55:01 +0200

    Added copying of input image
    Input image is being copied into data structure for faster access and modification (need to reaserach more for the best one for our purpose).

--- ImageToPathConverter.java
+++ ImageToPathConverter.java
@@ -1,4 +1,43 @@
 package com.dkrucze.PathifyCore;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+//Facade Class
 public class ImageToPathConverter {
+
+    //TODO Find better data structure to hold image?
+    private Color[][] inputImage;
+    private PathifiedImage result;
+
+    //Specify image to be converted
+    public ImageToPathConverter(BufferedImage image){
+        //Copy image into an array for easier processing
+        copyImage(image);
+    }
+
+    //Convert the image
+    public PathifiedImage convert(){
+        //Convert only once
+        if(result==null){
+            result=new PathifiedImage();
+        }
+
+        return result;
+    }
+
+    //Method for copying an image
+    private void copyImage(BufferedImage image){
+        //Get image dimensions and transparency
+        int h=image.getHeight(), w=image.getWidth();
+        boolean alpha = image.getColorModel().hasAlpha();
+
+        //Copy the image
+        inputImage=new Color[h][w];
+        for(int i=0;i<h;i++){
+            for(int j=0;j<w;j++){
+                inputImage[i][j]=new Color(image.getRGB(j,i),alpha);
+            }
+        }
+    }
 }
