9023ada8c2381b6fec3ef8ab005d3bc34f01e3c4
TheKiromen <TheKiromen@gmail.com>
2021-05-27 18:24:11 +0200

    Sobel Edge Detection
    Added basic structure for sobel edge detection

---
+++ SobelEdgeDetection.java
@@ -0,0 +1,17 @@
+package com.dkrucze.PathifyCore;
+
+public class SobelEdgeDetection {
+
+    private int threshold;
+    private SobelResult result;
+    private int[][] tmp;
+
+    public SobelEdgeDetection(int threshold){
+        this.threshold=threshold;
+    }
+
+    public SobelResult findEdges(int[][] input){
+        //TODO implement Sobel edge detection
+        return result;
+    }
+}
---
+++ SobelResult.java
@@ -0,0 +1,38 @@
+package com.dkrucze.PathifyCore;
+
+public class SobelResult {
+
+    private int[][] magnitude,xGradient,yGradient;
+
+    public SobelResult(){}
+    public SobelResult(int[][] magnitude,int[][] xGradient,int[][] yGradient){
+        this.magnitude=magnitude;
+        this.xGradient=xGradient;
+        this.yGradient=yGradient;
+    }
+
+    //Getters and setters
+    public int[][] getMagnitude() {
+        return magnitude;
+    }
+
+    public void setMagnitude(int[][] magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public int[][] getxGradient() {
+        return xGradient;
+    }
+
+    public void setxGradient(int[][] xGradient) {
+        this.xGradient = xGradient;
+    }
+
+    public int[][] getyGradient() {
+        return yGradient;
+    }
+
+    public void setyGradient(int[][] yGradient) {
+        this.yGradient = yGradient;
+    }
+}
