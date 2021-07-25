49b6d91282bbf1c0089f57f9cc6628726957e124
TheKiromen <TheKiromen@gmail.com>
2021-07-25 15:38:10 +0200

    Sobel Calculation Changes

--- SobelEdgeDetection.java
+++ SobelEdgeDetection.java
@@ -25,27 +25,23 @@ public class SobelEdgeDetection {
         for(int y=1;y<h-1;y++){
             for(int x=1;x<w-1;x++){
                 //Calculate xGradient
-                sumx=(int)(
-                        (input[y+1][x+1]&255)*0.25+(input[y][x+1]&255)*0.5+(input[y-1][x+1]&255)*0.25
-                       -(input[y+1][x-1]&255)*0.25-(input[y][x-1]&255)*0.5-(input[y-1][x-1]&255)*0.25
-                );
-                sumx=Math.abs(sumx);
+                sumx=(input[y+1][x+1]&255)*1+(input[y][x+1]&255)*2+(input[y-1][x+1]&255)*1-(input[y+1][x-1]&255)*1-(input[y][x-1]&255)*2-(input[y-1][x-1]&255)*1;
+                sumx=Math.abs(sumx)*255/1020;
                 if(sumx<threshold)
                     sumx=0;
                 xGradient[y][x]=(sumx << 16 | sumx << 8 | sumx)-16777216;
 
                 //Calculate yGradient
-                sumy=(int)(
-                        (input[y+1][x-1]&255)*0.25+(input[y+1][x]&255)*0.5+(input[y+1][x+1]&255)*0.25
-                       -(input[y-1][x-1]&255)*0.25-(input[y-1][x]&255)*0.5-(input[y-1][x+1]&255)*0.25
-                );
-                sumy=Math.abs(sumy);
+                sumy=(input[y+1][x-1]&255)*1+(input[y+1][x]&255)*2+(input[y+1][x+1]&255)*1-(input[y-1][x-1]&255)*1-(input[y-1][x]&255)*2-(input[y-1][x+1]&255)*1;
+                sumy=Math.abs(sumy)*255/1020;
                 if(sumy<threshold)
                     sumy=0;
                 yGradient[y][x]=(sumy << 16 | sumy << 8 | sumy)-16777216;
 
                 //Calculate magnitude
                 mag=(int)Math.sqrt(sumx*sumx+sumy*sumy);
+                if(mag>255)
+                    mag=255;
                 magnitude[y][x]=(mag << 16 | mag << 8 | mag)-16777216;
             }
         }
