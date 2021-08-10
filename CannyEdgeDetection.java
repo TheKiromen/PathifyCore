ae3f28b90e0beb8b94b42e867d41649eddfb7d7f
TheKiromen <TheKiromen@gmail.com>
2021-08-10 20:51:24 +0200

    Edge direction detection
    Implemented calculating angle of the edge and picking direction based on the edge.

--- CannyEdgeDetection.java
+++ CannyEdgeDetection.java
@@ -1,5 +1,7 @@
 package com.dkrucze.PathifyCore;
 
+import java.awt.*;
+
 public class CannyEdgeDetection {
 
     private int[][] sobelX,sobelY,sobelMag,result;
@@ -13,18 +15,75 @@ public class CannyEdgeDetection {
         sobelMag = sobel.getMagnitude();
         this.h=sobelMag.length;
         this.w=sobelMag[0].length;
-        result=new int[h][w];
         candidates=new boolean[h][w];
+        result=new int[h][w];
+        //Fill the result with black pixels;
+        int black = Color.BLACK.getRGB();
+        for(int y=0;y<h;y++){
+            for(int x=0;x<w;x++){
+                result[y][x]=black;
+            }
+        }
     }
 
     public int[][] detect(){
+        double angle;
+        int black = Color.BLACK.getRGB();
+        int white = Color.WHITE.getRGB();
 
         //Find all pixels above highThreshold
-        for(int y=0;y<h;y++){
-            for(int x=0;x<w;x++){
+        for(int y=1;y<h-1;y++){
+            for(int x=1;x<w-1;x++){
+                //atan() returns value from -PI/2 to PI/2, so I am adding PI/2,
+                //and converting to degrees for simplicity.
+                angle=Math.atan(sobelY[y][x]/sobelX[y][x])+Math.PI/2;
+                //New angle range is from 0 to 180 degrees (semicircle)
+                angle=Math.toDegrees(angle);
+                //Variables for finding local maximum
+                int xOffset=1,yOffset=1;
+                //Coordinates and value of the local maximum
+                int maxX, maxY, maxVal;
+
+                //Possible edge directions:
+                if(angle < 22.5 || angle > 157.5){
+                    //Horizontal ---
+                    maxX=x;
+                    maxY=y;
+                    maxVal=sobelMag[y][x]&255;
+
+                    //Check right side
+                    while(sobelMag[y+yOffset][x]!=black){
+                        if((sobelMag[y+yOffset][x]&255)>maxVal){
+                            maxY=y+yOffset;
+                            maxVal=sobelMag[y+yOffset][x]&255;
+                        }
+                    }
+                    yOffset=1;
+                    //Check left side
+                    while(sobelMag[y-yOffset][x]!=black){
+                        if((sobelMag[y-yOffset][x]&255)>maxVal){
+                            maxY=y-yOffset;
+                            maxVal=sobelMag[y+yOffset][x]&255;
+                        }
+                    }
+
+                    //Set pixel to white at local maximum coordinates if it is above highThreshold
+                    if(maxVal>highThreshold){
+                        result[maxY][maxX]=white;
+                    }
+
+                }else if(angle >= 22.5 && angle <= 67.5){
+                    //Right diagonal /
+
+                }else if(angle > 67.5 && angle < 112.5){
+                    //Vertical |
+
+                }else{//Angle between 112.5 and 157.5
+                    //Left diagonal \
+
+                }
+
                 //TODO
-                //-Calculate the angle of the pixel
-                //-Pick direction based on the angle
                 //-Check for local maximum
                 //-If above highThreshold set pixel in result to white.
                 //-If between two thresholds, mark it as a candidate
@@ -33,8 +92,8 @@ public class CannyEdgeDetection {
         }
 
         //Check all edge candidates
-        for(int y=0;y<h;y++){
-            for(int x=0;x<w;x++){
+        for(int y=1;y<h-1;y++){
+            for(int x=1;x<w-1;x++){
                 //TODO
                 //-Loop through all candidates
                 //-Check if a candidate is next to existing "hard" edge, if yes set pixel in result to white
